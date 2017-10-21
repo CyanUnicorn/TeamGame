dae2ae9253fa96d0a73caa456a0ff778ca8adf91
orantoohotty <32235746+orantoohotty@users.noreply.github.com>
2017-10-20 22:57:41 -0700

    oran

---
+++ TeamGame101/Assets/Scripts/powerup_spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup_spawn : MonoBehaviour {
+    public GameObject[] powerup;
+    public int arraymax;
+    public int arraynum;
+    public int triggervalue;
+    public int randomValue;
+    public int max;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        arraynum = Random.Range(0, arraymax);
+        randomValue = Random.Range(0, max);
+        if(triggervalue == randomValue)
+        {
+            Instantiate(powerup[arraynum], gameObject.transform);
+        }
+	}
+}
